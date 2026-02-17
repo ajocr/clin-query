325a41c508f1d7a72134c3dd1ace1170f6d380df
ajocr <yvonneocran12@gmail.com>
2026-02-17 10:43:53 -0800

    remove text

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,5 @@
   import "./index.css";
   import "./amplify-config";
 
-console.log('Region:', import.meta.env.VITE_AWS_REGION);
-console.log('User Pool ID:', import.meta.env.VITE_COGNITO_USER_POOL_ID);
-console.log('Client ID:', import.meta.env.VITE_COGNITO_CLIENT_ID);
-
   createRoot(document.getElementById("root")!).render(<App />);
   
